cf30f78d906b694b1a8f0a23976bafd1bdfbf646
Steven Greenwaters <swaters@livingstreets.com>
2016-11-14 20:32:52 -0500

    Discontinue vertical centering on homepage for mobile compatibility, and set viewport width to device width

--- header.php
+++ header.php
@@ -1,8 +1,12 @@
+<?php
+$RelativeToRoot = "";
+include $RelativeToRoot . 'visitor_tracker.php';
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Living Streets Consulting</title>
-	<meta name="viewport" content="initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta charset="utf-8">
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/livingstreets.css">
 	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
