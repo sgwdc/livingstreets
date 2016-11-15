529dad78b5f5efd30deeddc05349cce6db570336
Steven Greenwaters <swaters@livingstreets.com>
2016-11-14 20:40:07 -0500

    Add wp_head() to header, and remove obsolete code to move WordPress Toolbar to the bottom of the browser window

--- header.php
+++ header.php
@@ -10,5 +10,7 @@ include $RelativeToRoot . 'visitor_tracker.php';
 	<meta charset="utf-8">
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/livingstreets.css">
 	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+	<!-- Hook for plugins to insert code here (Avoids need to move WordPress Toolbar to the bottom of the browser window) -->
+	<?php wp_head(); ?>
 </head>
 <body bgcolor="#0067b5" <?php body_class(); ?>>
