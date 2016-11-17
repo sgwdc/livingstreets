3bb8a7e8efe478f1188673efc41007370ef1e6b5
Steven Greenwaters <swaters@livingstreets.com>
2016-11-17 12:43:37 -0500

    In header, add post name to HTML title, and make blog name dynamic

--- header.php
+++ header.php
@@ -5,7 +5,14 @@ include $RelativeToRoot . 'visitor_tracker.php';
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Living Streets Consulting</title>
+	<title><?php
+	// If this is NOT the front page, include the page title in the HTML title
+	if (!is_front_page()) {
+		wp_title('&raquo;', true, 'right');
+	}
+	// Add the blog name to the HTML title
+	bloginfo('name');
+	?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta charset="utf-8">
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/livingstreets.css">
