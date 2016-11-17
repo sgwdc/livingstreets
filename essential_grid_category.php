5a31bf3785f8b3603eb53994fc343d0ef78f5a0d
Steven Greenwaters <swaters@livingstreets.com>
2016-11-16 23:24:18 -0500

    First draft of a new WordPress template to show portfolio projects in the Essential Grid plugin only for a category specified in the query string (passed in from the "TagsLink" custom meta field in Essential Grid)

---
+++ essential_grid_category.php
@@ -0,0 +1,62 @@
+<?php
+/*
+Template Name: Essential Grid Category
+*/
+
+// Get the category "id" from the query string
+$requested_category = $_GET['id'];
+
+// Define arguments for WP_Query() 
+$args = array(
+	// Custom post type for the Essential Grid plugin
+	'post_type' => 'essential_grid',
+	// Custom taxonomy
+	'tax_query' => array(
+		array(
+			// Custom category for custom post type
+			'taxonomy' => 'essential_grid_category',
+			// NOTE: "field" can also be: term_id, name, or term_taxonomy_id (default is term_id)
+			'field'    => 'slug',
+			// NOTE: "terms" can also be an array
+			'terms'    => $requested_category
+		)
+	),
+	// Only return the post ID's
+	'fields' => 'ids'
+);
+// Run the query
+$query_result = new WP_Query( $args );
+/* dev
+echo '<pre>';
+print_r($query_result);
+echo '<pre>';
+*/
+
+// Pull out just the custom post ID's
+$post_ids = $query_result -> posts;
+
+// If no posts are found for this category (which should never happen)
+if (!count($post_ids)) {
+	// Send an HTTP status code 404, tell the user, and abort the script
+	header("HTTP/1.0 404 Not Found");
+	echo "Project category not found";
+	exit;
+}
+
+get_header();
+?>
+	<h2><img src="<?php bloginfo('template_directory'); ?>/images/livingstreets_logo_30px.png" align="absmiddle"> Living Streets Consulting</h2>
+
+	<?php
+		echo '<h1>' . $requested_category . ' Projects</h1>';
+		$essential_grid_posts_csv = implode(',', $post_ids);
+		echo do_shortcode('[ess_grid alias="portfolio" posts="' . $essential_grid_posts_csv . '"]');
+	?>
+
+	<h3>Other project categories:</h3>
+	<p>Insert here</p>
+
+	<p><a href="/">Home</a></p>
+	<p>&nbsp;</p>
+
+<?php get_footer(); ?>
