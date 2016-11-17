cbe43b8228cccecf4380187aba7f57b19f4fe17a
Steven Greenwaters <swaters@livingstreets.com>
2016-11-17 12:52:06 -0500

    Remove div#bodycontainer since we're no longer vertical centering homepage

--- home.php
+++ home.php
@@ -1,25 +1,22 @@
-<!-- This is used to vertical align everything -->
-<div id="bodycontainer">
-	<!-- Moved to individual pages so it can be center or left aligned:
-	<h2>Living Streets Consulting</h2>
-	-->
-	<?php get_header(); ?>
+<!-- Moved to individual pages so it can be center or left aligned:
+<h2>Living Streets Consulting</h2>
+-->
+<?php get_header(); ?>
 
-		<!-- This entire block will be expanded into the margin so it runs across the entire screen -->
-		<div id="fotoliacontainer">
-			<div class="divider">&nbsp;</div>
-			<!-- This will be used to clip the Fotolia image at 200px -->
-			<div id="fotoliainnercontainer">
-				<!-- The Fotolia image will be at least 200px, and full width -->
-				<img id="fotolia" src="http://livingstreets.com/wp-content/uploads/2016/09/Fotolia_48180217_XL_v4.jpg">
-			</div>
-			<div class="divider">&nbsp;</div>
+	<!-- This entire block will be expanded into the margin so it runs across the entire screen -->
+	<div id="fotoliacontainer">
+		<div class="divider">&nbsp;</div>
+		<!-- This will be used to clip the Fotolia image at 200px -->
+		<div id="fotoliainnercontainer">
+			<!-- The Fotolia image will be at least 200px, and full width -->
+			<img id="fotolia" src="http://livingstreets.com/wp-content/uploads/2016/09/Fotolia_48180217_XL_v4.jpg">
 		</div>
-		
-		<div class="clearfix">
-			<div style="float:left; /* dev:; border: 1px #f00 solid; */"><p>Interactive mapping web applications</p></div>
-			<div style="float:right; /* dev: border: 1px #f00 solid; */"><p><a href="/contact-form/">Contact</a></p></div>
-		</div>
+		<div class="divider">&nbsp;</div>
+	</div>
+	
+	<div class="clearfix">
+		<div style="float:left; /* dev:; border: 1px #f00 solid; */"><p>Interactive mapping web applications</p></div>
+		<div style="float:right; /* dev: border: 1px #f00 solid; */"><p><a href="/contact-form/">Contact</a></p></div>
+	</div>
 
-	<?php get_footer(); ?>
-</div>
+<?php get_footer(); ?>
