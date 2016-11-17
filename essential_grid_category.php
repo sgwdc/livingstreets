240b662ca2cb4eff7d84cbbda7eb8df93c387cd3
Steven Greenwaters <swaters@livingstreets.com>
2016-11-17 00:05:35 -0500

    Portfolio project categories template: Use get_term_by() to display the name of the custom category in the Essential Grid custom taxonomy

--- essential_grid_category.php
+++ essential_grid_category.php
@@ -1,10 +1,14 @@
 <?php
 /*
 Template Name: Essential Grid Category
+
+This is the template used to display portfolio project categories.
 */
 
 // Get the category "id" from the query string
-$requested_category = $_GET['id'];
+$category_slug = $_GET['id'];
+$field = 'slug';
+$taxonomy = 'essential_grid_category';
 
 // Define arguments for WP_Query() 
 $args = array(
@@ -14,11 +18,11 @@ $args = array(
 	'tax_query' => array(
 		array(
 			// Custom category for custom post type
-			'taxonomy' => 'essential_grid_category',
-			// NOTE: "field" can also be: term_id, name, or term_taxonomy_id (default is term_id)
-			'field'    => 'slug',
-			// NOTE: "terms" can also be an array
-			'terms'    => $requested_category
+			'taxonomy' => $taxonomy,
+			// Specify that the "terms" below is the slug for the requested category - NOTE: "field" can also be: term_id, name, or term_taxonomy_id (default is term_id)
+			'field' => $field,
+			// Specify the slug for the category - NOTE: "terms" can also be an array
+			'terms' => $category_slug
 		)
 	),
 	// Only return the post ID's
@@ -53,8 +57,12 @@ get_header();
 		the_content();
 		endwhile;
 
-		echo '<h1>' . $requested_category . ' Projects</h1>';
+		// Get the name of the category
+		$category_name = get_term_by($field, $category_slug, $taxonomy) -> name;
+		echo '<h1>Projects: ' . $category_name . '</h1>';
+		// Convert the list of custom posts to a comma separated string
 		$essential_grid_posts_csv = implode(',', $post_ids);
+		// Insert the "Essential Grid" plugin, and pass in the list of posts to display
 		echo do_shortcode('[ess_grid alias="portfolio" posts="' . $essential_grid_posts_csv . '"]');
 	?>
 
