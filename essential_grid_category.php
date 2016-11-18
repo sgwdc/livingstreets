f4eaf0366092123bfba9792f142d97d1268341e2
Steven Greenwaters <swaters@livingstreets.com>
2016-11-18 00:26:32 -0500

    Spacing only

--- essential_grid_category.php
+++ essential_grid_category.php
@@ -44,84 +44,84 @@ if (!count($post_ids)) {
 
 get_header();
 
-		/* Not sure whether we'll use this or not
-		// Display page content
-		while ( have_posts() ) : the_post();
-		the_content();
-		endwhile;
-		*/
+/* Not sure whether we'll use this or not
+// Display page content
+while ( have_posts() ) : the_post();
+the_content();
+endwhile;
+*/
 
-		// Get the name of the category
-		$category_name = get_term_by($field, $category_slug, $taxonomy) -> name;
-		echo '<h4><a href="/portfolio/">Portfolio of Steven Greenwaters</a></h4>';
+// Get the name of the category
+$category_name = get_term_by($field, $category_slug, $taxonomy) -> name;
+echo '<h4><a href="/portfolio/">Portfolio of Steven Greenwaters</a></h4>';
 
-		echo '<h2>Project category: ' . $category_name . '</h2>';
+echo '<h2>Project category: ' . $category_name . '</h2>';
 
-	/********************************************************************************/
-	/* DISPLAY PROJECTS IN THE CURRENT CATEGORY										*/
-	/********************************************************************************/
-		echo '<h4>Please select a project:</h4>';
+/********************************************************************************/
+/* DISPLAY PROJECTS IN THE CURRENT CATEGORY										*/
+/********************************************************************************/
+echo '<h4>Please select a project:</h4>';
 
-		// Convert the list of custom posts to a comma separated string
-		$essential_grid_posts_csv = implode(',', $post_ids);
-		// Insert the "Essential Grid" plugin, and pass in the list of posts to display
-		echo do_shortcode('[ess_grid alias="portfolio" posts="' . $essential_grid_posts_csv . '"]');
+// Convert the list of custom posts to a comma separated string
+$essential_grid_posts_csv = implode(',', $post_ids);
+// Insert the "Essential Grid" plugin, and pass in the list of posts to display
+echo do_shortcode('[ess_grid alias="portfolio" posts="' . $essential_grid_posts_csv . '"]');
 
-	/********************************************************************************/
-	/* END: DISPLAY PROJECTS IN THE CURRENT CATEGORY								*/
-	/********************************************************************************/
+/********************************************************************************/
+/* END: DISPLAY PROJECTS IN THE CURRENT CATEGORY								*/
+/********************************************************************************/
 
-	/********************************************************************************/
-	/* DISPLAY OTHER PROJECT CATEGORIES												*/
-	/********************************************************************************/
-	?>
-	<br>
-	<h4>Other project categories:</h4>
-	<?php
-	/* NOTE: THE SLUGS FOR THE ESSENTIAL GRID CATEGORIES MUST MATCH THE ASSOCIATED ESSENTIAL GRID CUSTOM POSTS FOR THOSE CATEGORIES */
-	// Define arguments for WP_Query() 
-	$args = array(
-		// Custom post type for the Essential Grid plugin
-		'post_type' => $post_type,
-		// Custom taxonomy
-		'tax_query' => array(
-			array(
-				// Custom category for custom post type
-				'taxonomy' => $taxonomy,
-				// Specify that the "terms" below is the slug for the requested category - NOTE: "field" can also be: term_id, name, or term_taxonomy_id (default is term_id)
-				'field' => $field,
-				// Specify the slug for the category - NOTE: "terms" can also be an array
-				'terms' => '0-categories'
-			)
+/********************************************************************************/
+/* DISPLAY OTHER PROJECT CATEGORIES												*/
+/********************************************************************************/
+?>
+<br>
+<h4>Other project categories:</h4>
+<?php
+/* NOTE: THE SLUGS FOR THE ESSENTIAL GRID CATEGORIES MUST MATCH THE ASSOCIATED ESSENTIAL GRID CUSTOM POSTS FOR THOSE CATEGORIES */
+// Define arguments for WP_Query() 
+$args = array(
+	// Custom post type for the Essential Grid plugin
+	'post_type' => $post_type,
+	// Custom taxonomy
+	'tax_query' => array(
+		array(
+			// Custom category for custom post type
+			'taxonomy' => $taxonomy,
+			// Specify that the "terms" below is the slug for the requested category - NOTE: "field" can also be: term_id, name, or term_taxonomy_id (default is term_id)
+			'field' => $field,
+			// Specify the slug for the category - NOTE: "terms" can also be an array
+			'terms' => '0-categories'
 		)
-	);
-	// Run the query
-	$query_result = new WP_Query( $args );
-	$category_posts = $query_result -> posts;
+	)
+);
+// Run the query
+$query_result = new WP_Query( $args );
+$category_posts = $query_result -> posts;
 
-	/* REMOVE THE CURRENT CATEGORY FROM THE LIST */
-	$category_post_ids = array();
-	// Iterate through the posts for Essential Grid categories
-	for ($i=0; $i < count($category_posts); $i++) {
-		// If this is a category other than the one currently being displayed
-		if ($category_posts[$i] -> post_name != $category_slug) {
-			array_push($category_post_ids, $category_posts[$i] -> ID);
-		}
+/* REMOVE THE CURRENT CATEGORY FROM THE LIST */
+$category_post_ids = array();
+// Iterate through the posts for Essential Grid categories
+for ($i=0; $i < count($category_posts); $i++) {
+	// If this is a category other than the one currently being displayed
+	if ($category_posts[$i] -> post_name != $category_slug) {
+		array_push($category_post_ids, $category_posts[$i] -> ID);
 	}
+}
 
-	// Convert the list of custom posts to a comma separated string
-	$category_post_ids_csv = implode(',', $category_post_ids);
+// Convert the list of custom posts to a comma separated string
+$category_post_ids_csv = implode(',', $category_post_ids);
 
-	// Insert the "Essential Grid" plugin, and pass in the list of posts to display
-	echo do_shortcode('[ess_grid alias="portfolio2" posts="' . $category_post_ids_csv . '"]');
-	/********************************************************************************/
-	/* END: DISPLAY OTHER PROJECT CATEGORIES										*/
-	/********************************************************************************/
-	?>
+// Insert the "Essential Grid" plugin, and pass in the list of posts to display
+echo do_shortcode('[ess_grid alias="portfolio2" posts="' . $category_post_ids_csv . '"]');
+/********************************************************************************/
+/* END: DISPLAY OTHER PROJECT CATEGORIES										*/
+/********************************************************************************/
+?>
 
-	<br>
-	<p><a href="/portfolio/">Return to all projects</a></p>
-	<p><a href="/">Home</a></p>
-	<p>&nbsp;</p>
+<br>
+<p><a href="/portfolio/">Return to all projects</a></p>
+<p><a href="/">Home</a></p>
+<p>&nbsp;</p>
 
 <?php get_footer(); ?>
--- single-essential_grid.php
+++ single-essential_grid.php
@@ -1,116 +1,116 @@
 <?php
-	// This file is automatically loaded when an individual project post is displayed
+// This file is automatically loaded when an individual project post is displayed
 
-	// Get all the categories this project post is in
-	$taxonomy = 'essential_grid_category';
-	global $post;
-	// Run the query
-	$custom_categories_array = wp_get_object_terms( $post->ID, $taxonomy);
-	// Create an associative array of the categories for easy access
-	$custom_categories_ass = array();
-	for ($i = 0; $i < count($custom_categories_array); $i++) {
-		$category_slug = $custom_categories_array[$i] -> slug;
-		$category_name = $custom_categories_array[$i] -> name;
-		$custom_categories_ass[$category_slug] = $category_name;
-	}
+// Get all the categories this project post is in
+$taxonomy = 'essential_grid_category';
+global $post;
+// Run the query
+$custom_categories_array = wp_get_object_terms( $post->ID, $taxonomy);
+// Create an associative array of the categories for easy access
+$custom_categories_ass = array();
+for ($i = 0; $i < count($custom_categories_array); $i++) {
+	$category_slug = $custom_categories_array[$i] -> slug;
+	$category_name = $custom_categories_array[$i] -> name;
+	$custom_categories_ass[$category_slug] = $category_name;
+}
 ?>
 
-<?php get_header(); ?>
+<?php
+get_header();
 
-	<?php
-		// Start the loop
-		while ( have_posts() ) : the_post();
-			// Get the referring URL, if it exists
-			$referring_url = $_SERVER['HTTP_REFERER'];
-			// If the referring URL is a strong (instead of NULL)
-			if (gettype($referring_url) == "string") {
-				// Get only the part after "?id="
-				preg_match("/^.+?\\?id=(.+)$/is" , $referring_url, $match);
-				$selected_category_slug = $match[1];
+// Start the loop
+while ( have_posts() ) : the_post();
+	// Get the referring URL, if it exists
+	$referring_url = $_SERVER['HTTP_REFERER'];
+	// If the referring URL is a strong (instead of NULL)
+	if (gettype($referring_url) == "string") {
+		// Get only the part after "?id="
+		preg_match("/^.+?\\?id=(.+)$/is" , $referring_url, $match);
+		$selected_category_slug = $match[1];
+	}
+
+	echo '<h4><a href="/portfolio/">Portfolio of Steven Greenwaters</a></h4>';
+
+	echo '<h2 class="project-title">';
+	// Display the project title
+	echo get_the_title();
+	// Link to the "Edit" page if the user has access
+	edit_post_link(
+		sprintf(
+			__( 'Edit project' ),
+			get_the_title()
+		),
+		'<span class="edit-link"> &nbsp;&nbsp; [ ',
+		' ]</span>'
+	);
+	echo '</h2>';
+
+	echo '<h5 class="categories">Categories: &nbsp;';
+	// Iterate through each category that contains this project post
+	$i = 0;
+	foreach ($custom_categories_ass as $category_slug => $category_name) {
+		// If we don't know which category was selected, or if this is a category other than the one selected
+		//if (!isset($selected_category_slug) || $selected_category_slug != $category_slug) {
+			// If this is not the first category displayed, add a divider
+			if ($i != 0) {
+				echo ' &nbsp;|&nbsp; ';
 			}
-
-			echo '<h4><a href="/portfolio/">Portfolio of Steven Greenwaters</a></h4>';
-
-			echo '<h2 class="project-title">';
-			// Display the project title
-			echo get_the_title();
-			// Link to the "Edit" page if the user has access
-			edit_post_link(
-				sprintf(
-					__( 'Edit project' ),
-					get_the_title()
-				),
-				'<span class="edit-link"> &nbsp;&nbsp; [ ',
-				' ]</span>'
-			);
-			echo '</h2>';
-
-			echo '<h5 class="categories">Categories: &nbsp;';
-			// Iterate through each category that contains this project post
-			$i = 0;
-			foreach ($custom_categories_ass as $category_slug => $category_name) {
-				// If we don't know which category was selected, or if this is a category other than the one selected
-				//if (!isset($selected_category_slug) || $selected_category_slug != $category_slug) {
-					// If this is not the first category displayed, add a divider
-					if ($i != 0) {
-						echo ' &nbsp;|&nbsp; ';
-					}
-					$category_url = '/portfolio/category/?id=' . $category_slug;
-					$category_name = $custom_categories_ass[$category_slug];
-					echo '<a href="' . $category_url . '"';
-					/* Unfortunately this is probably confusing to users
-					// If we don't know which category was selected, or if this is a category other than the one selected
-					if (!isset($selected_category_slug) || $selected_category_slug != $category_slug) {
-						// Disable bold
-						echo ' style="font-weight:normal;"';
-					}
-					*/
-					echo ' style="font-weight:normal;"';
-					echo '>' . $category_name . '</a>';
-				//}
-				// Increment the index
-				$i++;
+			$category_url = '/portfolio/category/?id=' . $category_slug;
+			$category_name = $custom_categories_ass[$category_slug];
+			echo '<a href="' . $category_url . '"';
+			/* Unfortunately this is probably confusing to users
+			// If we don't know which category was selected, or if this is a category other than the one selected
+			if (!isset($selected_category_slug) || $selected_category_slug != $category_slug) {
+				// Disable bold
+				echo ' style="font-weight:normal;"';
 			}
-			echo '</h5>';
-			?>
-
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<?php
-				/* Not sure whether we'll use this or not
-					the_excerpt();
-				*/
-				?>
-
-				<!-- Display the post content -->
-				<div class="entry-content">
-					<br>
-					<p><strong>Description:</strong></p>
-					<?php
-						the_content();
-					?>
-				</div>
-
-				<!-- Display the project's Featured Image -->
-				<div class="post-thumbnail">
-					<?php the_post_thumbnail(); ?>
-				</div>
-			</article><!-- #post-## -->
-
-		<?php
-		// End of the loop
-		endwhile;
-
-		// If we know the name of the referring category page, display and link to it
-		if (isset($selected_category_slug)) {
-			// Link to this category
-			$category_url = '/portfolio/category/?id=' . $selected_category_slug;
-			echo '<p><a href="' . $category_url . '">Return to all ' . $custom_categories_ass[$selected_category_slug] . ' projects</a></p>';
-		}
+			*/
+			echo ' style="font-weight:normal;"';
+			echo '>' . $category_name . '</a>';
+		//}
+		// Increment the index
+		$i++;
+	}
+	echo '</h5>';
 	?>
 
-	<p><a href="/portfolio/">Return to all projects</a></p>
-	<p><a href="/">Home</a></p>
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<?php
+		/* Not sure whether we'll use this or not
+			the_excerpt();
+		*/
+		?>
 
-	<p>&nbsp;</p>
+		<!-- Display the post content -->
+		<div class="entry-content">
+			<br>
+			<p><strong>Description:</strong></p>
+			<?php
+				the_content();
+			?>
+		</div>
+
+		<!-- Display the project's Featured Image -->
+		<div class="post-thumbnail">
+			<?php the_post_thumbnail(); ?>
+		</div>
+	</article><!-- #post-## -->
+
+<?php
+// End of the loop
+endwhile;
+
+// If we know the name of the referring category page, display and link to it
+if (isset($selected_category_slug)) {
+	// Link to this category
+	$category_url = '/portfolio/category/?id=' . $selected_category_slug;
+	echo '<p><a href="' . $category_url . '">Return to all ' . $custom_categories_ass[$selected_category_slug] . ' projects</a></p>';
+}
+?>
+
+<p><a href="/portfolio/">Return to all projects</a></p>
+<p><a href="/">Home</a></p>
+
+<p>&nbsp;</p>
 
 <?php get_footer(); ?>
