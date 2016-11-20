e65d3512d101de98749e40b20b2d6767c98adf85
Steven Greenwaters <swaters@livingstreets.com>
2016-11-20 14:16:13 -0500

    In 404 template, if the user was requesting a page within my portfolio (e.g. a category with no associated page), redirect to portfolio homepage

--- 404.php
+++ 404.php
@@ -1,3 +1,12 @@
+<?php
+// If the user was trying to access a page within my online portfolio
+if (substr($_SERVER['REQUEST_URI'], 0, 10) == "/portfolio") {
+	// Redirect to the portfolio homepage
+	header("Location: " . $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/portfolio/');
+	exit;
+}
+?>
+
 <?php get_header(); ?>
 
 	Page not found
