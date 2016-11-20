a7756de86048d711fe19cf41073b5212daff6f64
Steven Greenwaters <swaters@livingstreets.com>
2016-11-20 18:30:22 -0500

    In header, add 3px white border to company logo. Consolidate link for the company name and logo using jQuery events. Remove link and events when already on the homepage. Make company name not selectable.

--- header.php
+++ header.php
@@ -20,13 +20,42 @@ include $RelativeToRoot . 'visitor_tracker.php';
 	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico">
 	<!-- Hook for plugins to insert code here (Avoids need to move WordPress Toolbar to the bottom of the browser window) -->
 	<?php wp_head(); ?>
+	<?php
+		if ($_SERVER['REQUEST_URI'] == "/") $isHomepage = true; else $isHomepage = false;
+		if (!$isHomepage) {
+		?>
+		<script>
+			// Fire when jQuery has finished loading
+			jQuery( document ).ready(function() {
+				// Define events for company name and logo
+				jQuery( "div#living-streets-header" )
+					.on('mouseenter mouseup', function() {
+						jQuery( this ).find( 'img' ).css("border-color", "#88cbfa");
+					})
+					.on('mouseleave', function() {
+						jQuery( this ).find( 'img' ).css("border-color", "#fff");
+					})
+					.on('mousedown', function() {
+						jQuery( this ).find( 'img' ).css("border-color", "#f00");
+					})
+			});
+		</script>
+	<?php
+		}
+	?>
 </head>
 <body bgcolor="#0067b5" <?php body_class(); ?>>
+	<?php
+		if (!$isHomepage) echo '<a href="/">';
+	?>
 	<div id="living-streets-header" class="clearfix">
 		<div>
-			<a href="/"><img src="<?php bloginfo('template_directory'); ?>/images/livingstreets_logo_100px.png"></a>
+			<img src="<?php bloginfo('template_directory'); ?>/images/livingstreets_logo_100px.png">
 		</div>
 		<div>
-			<h1><a href="/"><?php echo get_bloginfo('name'); ?></a></h1>
+			<h1><?php echo get_bloginfo('name'); ?></h1>
 		</div>
 	</div>
+	<?php
+		if (!$isHomepage) echo '</a>';
+	?>
