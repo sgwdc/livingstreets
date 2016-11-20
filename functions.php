6dfaf2abb15977e37fca45ceb5ebe2640489cb64
Steven Greenwaters <swaters@livingstreets.com>
2016-11-20 13:17:15 -0500

    Remove unnecessary WordPress admin menu items

--- functions.php
+++ functions.php
@@ -12,4 +12,15 @@ add_action( 'init', 'add_taxonomies_to_pages' );
 // Remove stupid emoji icons
 remove_action('wp_head', 'print_emoji_detection_script', 7);
 remove_action('wp_print_styles', 'print_emoji_styles');
+
+// Remove unnecessary WordPress admin menu items
+function remove_menus() {
+	// Posts
+	remove_menu_page( 'edit.php' );
+	// Comments
+	remove_menu_page( 'edit-comments.php' );
+	// Essential Grid
+	remove_menu_page( 'edit.php?post_type=essential_grid' );
+}
+add_action( 'admin_menu', 'remove_menus' );
 ?>
