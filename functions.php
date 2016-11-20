3633113b98f3eb4073feaf498213def75b653427
Steven Greenwaters <swaters@livingstreets.com>
2016-11-20 12:34:44 -0500

    Remove stupid emoji icons from HTML

--- functions.php
+++ functions.php
@@ -8,4 +8,8 @@ function add_taxonomies_to_pages() {
 	register_taxonomy_for_object_type( 'category', 'page' );
 }
 add_action( 'init', 'add_taxonomies_to_pages' );
+
+// Remove stupid emoji icons
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
 ?>
