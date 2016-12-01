62ae9793e49b153223e84bebc344e2565cd8d7ee
Steven Greenwaters <swaters@livingstreets.com>
2016-12-01 12:59:28 -0500

    Workaround for Adobe Acrobat messing up a URL in my resume

--- 404.php
+++ 404.php
@@ -1,6 +1,12 @@
 <?php
+// Workaround for Adobe Acrobat mysteriously converting a simple "-" (dash) into "%E2%80%90" (Use substr() since we don't know if there will be a trailing slash)
+if (substr($_SERVER['REQUEST_URI'], 0, 33) == "/portfolio/gis%E2%80%90geospatial") {
+	// Redirect to the correct URL
+	header("Location: " . $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/portfolio/gis-geospatial/');
+	exit;
+
 // If the user was trying to access a page within my online portfolio
-if (substr($_SERVER['REQUEST_URI'], 0, 10) == "/portfolio") {
+} else if (substr($_SERVER['REQUEST_URI'], 0, 10) == "/portfolio") {
 	// Redirect to the portfolio homepage
 	header("Location: " . $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/portfolio/');
 	exit;
