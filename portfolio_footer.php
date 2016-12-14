1eae33264f619b7d965275659b59a1803a566463
Steven Greenwaters <swaters@livingstreets.com>
2016-12-14 09:19:30 -0500

    Portfolio footer text change

--- portfolio_footer.php
+++ portfolio_footer.php
@@ -1,7 +1,9 @@
-<h4>Additional information:</h4>
+<h4>Contact info:</h4>
 
 <p>Please contact me by phone at (202) 643-2866, or via <a href="/contact">online contact form</a>.</p>
+<br>
 
+<h4>See my LinkedIn and GitHub profiles:</h4>
 <p><a href="http://www.linkedin.com/in/stevengreenwaters" target="_blank"><img src="https://static.licdn.com/scds/common/u/img/webpromo/btn_myprofile_160x33.png" alt="View Steven Greenwaters's profile on LinkedIn" width="160" height="33" align="absmiddle" border="0" /></a></p>
 
 <p><a href="https://github.com/sgwdc" target="_blank"><img src="/wp-content/uploads/2016/12/GitHub_logo_SGW_160px.png" /></a></p>
