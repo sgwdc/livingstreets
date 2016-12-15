403c3af4555147cca7462db3c554513dfdcf7843
Steven Greenwaters <swaters@livingstreets.com>
2016-12-15 15:33:10 -0500

    Text

--- portfolio_section.php
+++ portfolio_section.php
@@ -64,7 +64,7 @@ if ($post_status != "publish") {
 <h4><a href="/portfolio/">Portfolio of Steven Greenwaters</a></h4>
 <?php
 // Display the name of the current portfolio section
-echo '<h2>Project category: ' . $page_name;
+echo '<h2>Category: ' . $page_name;
 
 // Link to the "Edit" page if the user has access
 edit_post_link(
