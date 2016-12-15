b4ac0351c1fc021e4f0e8b58a28253279a231d17
Steven Greenwaters <swaters@livingstreets.com>
2016-12-15 11:00:36 -0500

    Move livingstreets.css up a level since it's the only non-WordPress CSS file

--- header.php
+++ header.php
@@ -15,7 +15,7 @@ include $RelativeToRoot . 'visitor_tracker.php';
 	?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta charset="utf-8">
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/livingstreets.css">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/livingstreets.css">
 	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
 	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico">
 	<!-- Hook for plugins to insert code here (Avoids need to move WordPress Toolbar to the bottom of the browser window) -->
