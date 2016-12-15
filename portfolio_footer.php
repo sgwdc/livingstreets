84f62fb352279594fc7e8205691971243a64c4e2
Steven Greenwaters <swaters@livingstreets.com>
2016-12-15 10:54:01 -0500

    Remove obsolete XHTML

--- portfolio_footer.php
+++ portfolio_footer.php
@@ -6,6 +6,6 @@
 <br>
 
 <h4>See my LinkedIn and GitHub profiles:</h4>
-<p><a href="http://www.linkedin.com/in/stevengreenwaters" target="_blank"><img src="https://static.licdn.com/scds/common/u/img/webpromo/btn_myprofile_160x33.png" alt="View Steven Greenwaters's profile on LinkedIn" width="160" height="33" align="absmiddle" border="0" /></a></p>
+<p><a href="http://www.linkedin.com/in/stevengreenwaters" target="_blank"><img src="https://static.licdn.com/scds/common/u/img/webpromo/btn_myprofile_160x33.png" alt="View Steven Greenwaters's profile on LinkedIn" width="160" height="33" align="absmiddle" border="0"></a></p>
 
-<p><a href="https://github.com/sgwdc" target="_blank"><img src="/wp-content/uploads/2016/12/GitHub_logo_SGW_160px.png" /></a></p>
+<p><a href="https://github.com/sgwdc" target="_blank"><img src="/wp-content/uploads/2016/12/GitHub_logo_SGW_160px.png"></a></p>
