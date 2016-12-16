37e13cd1b4e378ce0e89bad9cf313e5ca9fb071c
Steven Greenwaters <swaters@livingstreets.com>
2016-12-16 10:53:55 -0500

    Collapse portfolio footer so everything is on two lines. Add more spacing at the bottom of the page. And some small stuff.

--- portfolio_footer.php
+++ portfolio_footer.php
@@ -1,13 +1,11 @@
 <div class="section-divider"></div>
 
-<h4>Contact info:</h4>
+<h4>Contact me: <span class="non-bold">By phone at (202) 643-2866, or via <a href="/contact/">online contact form</a>.</span></h4>
 
-<p>Please contact me by phone at (202) 643-2866, or via <a href="/contact">online contact form</a>.</p>
-<br>
-
-<h4>See my LinkedIn and GitHub profiles:</h4>
-<p>
+<h4>See my LinkedIn and GitHub profiles:
+	&nbsp;
 	<a href="https://www.linkedin.com/in/stevengreenwaters" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/LinkedIn_button.png" class="profile-button" alt="View Steven Greenwaters' profile on LinkedIn"></a>
 	&nbsp; &nbsp;
 	<a href="https://github.com/sgwdc" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/GitHub_button.png" class="profile-button" alt="View Steven Greenwaters' profile on GitHub"></a>
-</p>
+</h4>
+<p>&nbsp;</p>
