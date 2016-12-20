19028485a3fe9b5eb06285f118ef7b2bd589a1af
Steven Greenwaters <swaters@livingstreets.com>
2016-12-19 19:56:51 -0500

    Footer adjustments

--- portfolio_footer.php
+++ portfolio_footer.php
@@ -1,11 +1,12 @@
 <br>
 <br>
-<h4>Contact me: <span class="non-bold">By phone at (202) 643-2866, or via <a href="/contact/">online contact form</a>.</span></h4>
-
-<h4>See my LinkedIn and GitHub profiles:
-	&nbsp;
-	<a href="https://www.linkedin.com/in/stevengreenwaters" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/LinkedIn_button.png" class="profile-button" alt="View Steven Greenwaters' profile on LinkedIn"></a>
-	&nbsp; &nbsp;
-	<a href="https://github.com/sgwdc" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/GitHub_button.png" class="profile-button" alt="View Steven Greenwaters' profile on GitHub"></a>
-</h4>
-<p>&nbsp;</p>
+<h3>More information:</h3>
+<ul>
+	<li><strong>Contact me:</strong> By phone at (202) 643-2866, or via <a href="/contact/">online contact form</a>.</li>
+	<li><strong>See my LinkedIn and GitHub profiles:</strong>
+		<br>
+		<a href="https://www.linkedin.com/in/stevengreenwaters" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/LinkedIn_button.png" class="profile-button" alt="View Steven Greenwaters' profile on LinkedIn"></a>
+		<br>
+		<a href="https://github.com/sgwdc" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/GitHub_button.png" class="profile-button" alt="View Steven Greenwaters' profile on GitHub"></a>
+	</li>
+</ul>
