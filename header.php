a33dc3eb5c5e2a4b85fa6834f2ae38425b6ac5e2
Steven Greenwaters <swaters@livingstreets.com>
2017-01-17 10:32:28 -0500

    Cache killer

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <?php
 // Set a version number to ensure any obsolete CSS and JS files are not cached
-$version = 1;
+$version = 3;
 
 $RelativeToRoot = "";
 include $RelativeToRoot . 'visitor_tracker.php';
