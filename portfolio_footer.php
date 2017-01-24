ffbfee7192e0d8174649bb17437f935dac51af1d
Steven Greenwaters <swaters@livingstreets.com>
2017-01-24 00:28:15 -0500

    Replace contact info, LinkedIn button, and GitHub button in portfolio footer to new portfolio-specific Contact page.

--- portfolio_footer.php
+++ portfolio_footer.php
@@ -1,17 +1,5 @@
 <br>
 <br>
-<h3>More information:</h3>
-<ul>
-	<li><strong>Contact me:</strong> By phone at (202) 643-2866, or via <a href="/contact/">online contact form</a>.</li>
-	<li><strong>See my LinkedIn and GitHub profiles:</strong>
-		<br>
-		<a href="https://www.linkedin.com/in/stevengreenwaters" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/LinkedIn_button.png" class="profile-button" alt="View Steven Greenwaters' profile on LinkedIn"></a>
-		<br>
-		<a href="https://github.com/sgwdc" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/GitHub_button.png" class="profile-button" alt="View Steven Greenwaters' profile on GitHub"></a>
-	</li>
-</ul>
-<br>
-<br>
 <?php
 	// Show the dropdown menu (Note this standard WordPress menu gets overridden by the Max Mega Menu plugin)
 	wp_nav_menu(
